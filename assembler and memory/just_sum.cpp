e3e854f4a812e8cbd5bf2c05a1b0e8515fbbb5f8
ash <fomin.iva2017@gmail.com>
2023-05-28 21:29:33 +0300

    It was so easy.

---
+++ assembler and memory/just_sum.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ash on 28.05.23.
+//
+
+#include <iostream>
+
+extern "C" long long sum1(long long, long long);
+extern "C" long long sum2(long long, long long);
+
+asm(R"(
+sum1:
+    mov %rdi, %rax
+    add %rsi, %rax
+    ret
+
+sum2:
+    lea (%rsi, %rdi), %rax
+    ret
+)");
+
+int main() {
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << sum1(a, b) << " " <<sum2(a, b) << std::endl;
+}
