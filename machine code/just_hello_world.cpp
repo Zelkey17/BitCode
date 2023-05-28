86883e45154fb2ec03c4ffe47c2d3a936da848a0
ash <fomin.iva2017@gmail.com>
2023-05-28 21:13:51 +0300

    I hope I never see it again...

--- machine code/just_hello_world.cpp
+++ machine code/just_hello_world.cpp
@@ -6,36 +6,36 @@
 
 
 int main() {
-
     auto *arr = new uint8_t[28]{
-            0x89,
-            0xf2,
+            0x89, // mov
+            0xf2, // rsi -> rdx
 
-            0x48,
+            0x48, // something magic
 
-            0x89,
-            0xfe,
+            0x89, // mov
+            0xfe, // rdi -> rsi
 
-            0x48,
+            0x48, // something magic
 
-            0xc7,
-            0xc7,
-            0x00, 0x00, 0x00, 0x00,
+            0xc7, // mov (data to reg)
+            0xc7, // reg = rdi
+            0x00, 0x00, 0x00, 0x00, // data = $1
 
-            0x48,
+            0x48, // something magic
 
-            0xc7,
-            0xc0,
-            0x01, 0x00, 0x00, 0x00,
+            0xc7, // mov (data to reg)
+            0xc0, // reg = rax
+            0x01, 0x00, 0x00, 0x00, // data = $0
 
-            0x0f, 0x05,
+            0x0f, 0x05, // syscall
 
-            0xc3
+            0xc3 // ret
+
     }; //generating array of bytes
 
     auto write = reinterpret_cast<void (*)(char *, long long)>(arr); // getting function from the array
 
-    chmod(write, "rwe");
+    chmod(write, "rwe"); // change page permission
 
-    write("Hello, world!\n", 14);
+    write("Hello, world!\n", 14); // ITS WORKING
 }
